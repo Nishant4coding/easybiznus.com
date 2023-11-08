fa388ad8cc1489d9664b83ba8aaf6330687c1314
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2023-11-08 17:31:53 +0530

    pin code input fixed

--- src/components/ProductPage/Pincode.jsx
+++ src/components/ProductPage/Pincode.jsx
@@ -6,16 +6,14 @@ import { IonIcon } from '@ionic/react';
 
 const Pincode = () => {
     return (
-        <Box sx={{width:"100%"}}>
-            <Typography sx={{fontWeight:"700", fontSize:"13px", marginBottom:"15px"}}>Please enter the PIN code to check the delivery time</Typography>
-            <Stack direction={"row"} sx={{justifyContent:"space-between", width:"100%"}}>
-                <Stack direction="row" sx={{ position: "relative", width:"70%"}}>
-                    <TextField id="outlined-basic" label="PIN code" variant="outlined" className={styles.qtyinput} sx={input}>
-                    </TextField>
-                    <IonIcon icon={closeCircleOutline} size={"medium"} style={{ position: "absolute", top: "32%", right: "10px", cursor: "pointer" }}
-                    ></IonIcon>
+        <Box sx={{ width: "100%",}}>
+            <Typography sx={{ fontWeight: "700", fontSize: "13px", marginBottom: "15px" }}>Please enter the PIN code to check the delivery time</Typography>
+            <Stack direction={"row"} sx={{ justifyContent: "space-between", width: "100%", }} gap={1}>
+                <Stack direction="row" sx={{ position: "relative", width: "70%"}}>
+                    <TextField id="outlined-basic" label="PIN code" variant="outlined" className={styles.qtyinput} sx={input} style={{width:'100%'}}></TextField>
+                    <IonIcon icon={closeCircleOutline} size={"medium"} style={{ position: "absolute", top: "32%", right: "10px", cursor: "pointer" }}></IonIcon>
                 </Stack>
-                <Box className={styles.button} style={{width:"100px"}}>
+                <Box className={styles.button} style={{ width: "100px" }}>
                     CHECK
                 </Box>
             </Stack>
@@ -30,14 +28,13 @@ const input = {
         color: '#0D1A26',
         fontWeight: "700",
         marginTop: "0px",
-        fontSize:"10px"
+        fontSize: "10px"
     },
     '& .MuiInput-underline:after': {
         borderBottomColor: '#B2BAC2',
     },
     '& .MuiOutlinedInput-root': {
         height: "45px",
-        width:"248px",
         '& fieldset': {
             border: '1px solid #0D1A26',
             borderRadius: "10px",
@@ -53,6 +50,6 @@ const input = {
     '& label': {
         marginTop: "-4px",
         color: '#0D1A26',
-        fontSize:"13px"
+        fontSize: "13px"
     }
 };
