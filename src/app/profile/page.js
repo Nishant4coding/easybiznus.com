9f0d25e4ba3f407bb7e32cd312edab99cc05f74f
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2023-11-19 20:06:47 +0530

    Profile Page Done

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,19 +1,27 @@
 "use client"
 import BasicBreadcrumbs from '@/components/BreadCrumbs'
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import Footer from '@/components/Footer/Footer';
 import Profile from '@/components/Account/Profile';
+import global from '@/global.module.css';
+import ProfileMobile from '@/components/Account/Mobile/Profile';
 
 const ProfilePage = () => {
   return (
     <>
-      <BasicBreadcrumbs inactive={inactive} active="My Profile" fsize={"22px"} mt={"20px"} />
-      <Stack sx={{ width: '80%', margin: '10px auto', alignItems: 'center'}}>
-        <Typography sx={{ fontSize: '50px', fontWeight: '700', textAlign: 'center', marginBottom: '30px' }}>My Profile</Typography>
-          <Profile/>
-      </Stack>
-      <Footer />
+      <Box className={global.desktop}>
+        <BasicBreadcrumbs inactive={inactive} active="My Profile" fsize={"22px"} mt={"20px"} />
+        <Stack sx={{ width: '80%', margin: '10px auto', alignItems: 'center' }}>
+          <Typography sx={{ fontSize: '50px', fontWeight: '700', textAlign: 'center', marginBottom: '30px' }}>My Profile</Typography>
+          <Profile />
+        </Stack>
+        <Footer />
+      </Box>
+
+      <Box className={global.mobile} style={{ padding:'120px 15px 35px 15px', backgroundColor:'#EFEFEF'}}>
+        <ProfileMobile/>
+      </Box>
     </>
   )
 }
