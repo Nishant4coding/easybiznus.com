a1e8f18def012f704ea3813ef8361cbd66ab75eb
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2023-11-20 11:03:44 +0530

    setting page update

--- src/components/Account/Mobile/Setting.jsx
+++ src/components/Account/Mobile/Setting.jsx
@@ -1,22 +1,22 @@
 'use client'
 import { IonIcon } from '@ionic/react';
-import { Typography, Stack } from '@mui/material'
+import { Typography, Stack, Box } from '@mui/material'
 import { layersOutline } from 'ionicons/icons';
 import React from 'react';
-
+import { StyledSwitch } from '@/styled/Switch';
 
 const Setting = () => {
   return (
-    <Stack gap={5} sx={{width:'100%'}}>
+    <Stack gap={5} sx={{ width: '100%' }}>
       <Stack gap={0.7}>
-        <Typography sx={{fontWeight:'600', fontSize:'20px', marginBottom:'10px'}}>Notification Settings</Typography>
-        <Option title="Alerts"/>
-        <Option title="Notification"/>
+        <Typography sx={{ fontWeight: '600', fontSize: '20px', marginBottom: '10px' }}>Notification Settings</Typography>
+        <Option title="Alerts" sw={true}/>
+        <Option title="Notification" sw={true}/>
       </Stack>
 
       <Stack gap={0.7}>
-        <Typography sx={{fontWeight:'600', fontSize:'20px', marginBottom:'10px'}}>App Version</Typography>
-        <Option title="Version 1.0.0"/>
+        <Typography sx={{ fontWeight: '600', fontSize: '20px', marginBottom: '10px' }}>App Version</Typography>
+        <Option title="Version 1.0.0" />
       </Stack>
     </Stack>
   )
@@ -26,11 +26,16 @@ export default Setting
 
 
 
-const Option=({title})=>{
-    return(
-        <Stack gap={2} direction={"row"} sx={{position:'relative', padding:'15px', backgroundColor:'#fff', borderRadius:'5px', alignItems:'center'}}>
-            <IonIcon style={{fontSize:'25px'}} icon={layersOutline}/>
-            <Typography sx={{fontSize:'18px'}}>{title}</Typography>
-        </Stack>
-    )
+const Option = ({ title, sw }) => {
+  return (
+    <Stack gap={2} direction={"row"} sx={{ position: 'relative', padding: '15px', backgroundColor: '#fff', borderRadius: '5px', alignItems: 'center' }}>
+      <IonIcon style={{ fontSize: '25px' }} icon={layersOutline} />
+      <Typography sx={{ fontSize: '18px' }}>{title}</Typography>
+      {sw&&
+        <Box sx={{ width: 'auto', height: 'auto', position: 'absolute', right: '20px' }}>
+          <StyledSwitch />
+        </Box>
+      }
+    </Stack>
+  )
 }
