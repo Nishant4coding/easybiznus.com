a2098d6058c760bc6b5ca3477aef6806120fa7d9
Nishant4coding <srivastava4nishant@gmail.com>
2024-08-15 03:42:05 +0530

    register- api, slice

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,16 +1,16 @@
 "use client";
 
-import Login from "../../components/Login/Login.jsx";
-import LoginImage from "../../assets/svg/LoginPage.svg";
-import JoinButton from "../../assets/svg/Join_button.svg";
+import { Stack, Typography } from "@mui/material";
 import Image from "next/image";
-import { Stack, Box, Typography } from "@mui/material";
-import style from "../Global.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import { getProfile } from "@/Redux/Features/profile/profileSlice";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import JoinButton from "../../assets/svg/Join_button.svg";
+import LoginImage from "../../assets/svg/LoginPage.svg";
+import Login from "../../components/Login/Login.jsx";
+import style from "../Global.module.css";
 
 const LoginPage = () => {
   const router = useRouter();
@@ -18,9 +18,10 @@ const LoginPage = () => {
 
   useEffect(() => {
     if (profileState.profile) {
+      toast.error("Already Logged In, Please Logout First");
       router.push("/");
     }
-  }, [profileState.profile, router]);
+  });
 
   return (
     <Stack
